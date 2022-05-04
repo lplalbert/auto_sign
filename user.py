70872f4647e9ef2504a5694137887757bffec032
lplalbert <81011915+lplalbert@users.noreply.github.com>
2022-05-04 18:39:57 +0800

    Update user.py

--- user.py
+++ user.py
@@ -5,7 +5,7 @@
 @time :2022-05-04
 """
 
-UID = "202024100711" #学号
-PWD = ".200222Lpl\n" #密码
-wechat_path = r'D:\WeChat\WeChat.exe' #微信位置
-group = '文件传输助手'
+UID = "" # 学号
+PWD = "\n"  #密码 !!!不要删除\n!!!
+wechat_path = r'D:\WeChat\WeChat.exe' #微信位置 不知道的话右键点击微信图标打开文件所在位置，不要漏掉.exe
+group = '文件传输助手' #修改成群聊名称
